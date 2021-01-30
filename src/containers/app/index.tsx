ed7e6f1c173a2dabab8a48c147cdb72e09caa634
Alex Nelson <w.alexnelson@gmail.com>
2021-01-30 14:45:39 -0700

    add typescript

---
+++ src/containers/app/index.tsx
@@ -0,0 +1,23 @@
+import './app.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='app-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='app-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
